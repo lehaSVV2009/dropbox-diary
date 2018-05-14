e295521d7b56276531409aa75924536b0d2a4461
Alex Soroka <lehaSVV2009@gmail.com>
2018-05-15 00:38:43 +0300

    Fix docker login warning

--- dropbox-diary-api/heroku_deploy.sh
+++ dropbox-diary-api/heroku_deploy.sh
@@ -1,3 +1,3 @@
-docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
+echo "$HEROKU_TOKEN" | docker login -u _ --password-stdin registry.heroku.com
 docker build -t registry.heroku.com/dropbox-diary-api/web -f dropbox-diary-api/Dockerfile dropbox-diary-api/.
 docker push registry.heroku.com/dropbox-diary-api/web

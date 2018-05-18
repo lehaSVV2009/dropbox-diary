e25b2de39a1944964ca81b3b1b181fa56b2de868
Alex Soroka <lehaSVV2009@gmail.com>
2018-05-18 21:48:17 +0300

    Configure AWS Lambda to parse "new note" request and return valid response

--- dropbox-diary-function/src/main/java/kadiary/Main.java
+++ dropbox-diary-function/src/main/java/kadiary/Main.java
@@ -3,67 +3,97 @@ package kadiary;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Singular;
+import lombok.val;
 
 public class Main implements RequestStreamHandler {
 
-  private final ObjectMapper objectMapper = new ObjectMapper();
+  private static final String ERROR_MESSAGE =
+      "Invalid format. The request should send note like `POST { \"note\": \"Bla bla\"}`";
 
-  // TODO try to use lombok
+  private final ObjectMapper objectMapper =
+      new ObjectMapper()
+          // allow to return "2014-12-20T02:30" instead of { date: "12", month: "December", ... }
+          .registerModule(new JavaTimeModule())
+          .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+  @Data
   @JsonIgnoreProperties(ignoreUnknown = true)
-  public static class Request {
+  public static class RequestNote {
+    private final LocalDateTime date = LocalDateTime.now();
     private String note;
-
-    public String getNote() {
-      return note;
-    }
-
-    public void setNote(String note) {
-      this.note = note;
-    }
   }
 
-  // TODO try to use lombok builder
+  @Getter
+  @Builder
   public static class Response {
     private final int statusCode;
-    private final String body;
+    private final Object body;
+    @Singular
     private final Map<String, String> headers;
-
-    public Response(int statusCode, String body, Map<String, String> headers) {
-      this.statusCode = statusCode;
-      this.body = body;
-      this.headers = headers;
-    }
-
-    public int getStatusCode() {
-      return statusCode;
-    }
-
-    public String getBody() {
-      return body;
-    }
-
-    public Map<String, String> getHeaders() {
-      return headers;
-    }
   }
 
   @Override
-  public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-    // TODO probably wrap input and output by try with resources
-    Request request = objectMapper.readValue(input, Request.class);
+  public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+    try (val input = inputStream;
+         val output = outputStream) {
 
-    // TODO do some dropbox stuff
+      // Read http request in format { "note": "123" }
+      val requestNote = objectMapper.readValue(input, RequestNote.class);
+      context.getLogger().log("Request: " + requestNote);
 
-    Map<String, String> headers = new HashMap<>();
-    headers.put("Content-Type", "application/json");
-    Response response = new Response(201, request.toString(), headers);
-    objectMapper.writeValue(output, response);
+      // Handle request and response
+      val response = execute(requestNote);
+
+      // Write http response in format { "note": "123" }
+      context.getLogger().log("Response: " + response);
+      objectMapper.writeValue(output, response);
+
+    } catch (JsonParseException | JsonMappingException e) {
+      objectMapper.writeValue(outputStream, invalid());
+    }
+  }
+
+  private Response execute(RequestNote requestNote) {
+    // Return HTTP 400 if response format is invalid
+    if (requestNote == null || requestNote.getNote() == null) {
+      return invalid();
+    }
+
+    // TODO
+    // Save request in dropbox
+
+    // Respond with successfully saved request
+    return created(requestNote);
+  }
+
+  private static Response created(RequestNote note) {
+    return Response.builder()
+        .statusCode(201)
+        .body(note)
+        .header("Content-Type", "application/json")
+        .build();
+  }
+
+  private static Response invalid() {
+    return Response.builder()
+        .statusCode(400)
+        .body("{\"message\": \"" + ERROR_MESSAGE + "\"}")
+        .header("Content-Type", "application/json")
+        .build();
   }
 }

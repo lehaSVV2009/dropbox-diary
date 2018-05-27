2161cfbe1424127ed0718413d0e13ee5c8890229
Alex Soroka <lehaSVV2009@gmail.com>
2018-05-27 14:27:40 +0300

    Delete unused dropbox-diary-api

--- dropbox-diary-api/heroku_deploy.sh
+++
@@ -1,3 +0,0 @@
-echo "$HEROKU_TOKEN" | docker login -u _ --password-stdin registry.heroku.com
-docker build -t registry.heroku.com/dropbox-diary-api/web -f dropbox-diary-api/Dockerfile dropbox-diary-api/.
-docker push registry.heroku.com/dropbox-diary-api/web

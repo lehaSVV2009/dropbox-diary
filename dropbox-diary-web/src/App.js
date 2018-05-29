1bc1c50cb5637e25a81d4d6a90c8148a4c88dce8
Alex Soroka <lehaSVV2009@gmail.com>
2018-05-29 10:59:08 +0300

    Add message to UI after clicking save

--- dropbox-diary-web/src/App.js
+++ dropbox-diary-web/src/App.js
@@ -4,32 +4,56 @@ import { Button, Input, MessageList } from "react-chat-elements";
 import "react-chat-elements/dist/main.css";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [this.createMessage("Hello! Any interesting story?", "left")],
+      text: ""
+    };
+  }
+
+  handleInputChange = ({ target: { value } }) => this.setState({ text: value });
+
+  handleSendClick = () => {
+    const { text, messages } = this.state;
+    if (!text) {
+      return;
+    }
+
+    // Add new message to UI
+    messages.push(this.createMessage(text));
+    this.setState({
+      text: "",
+      messages
+    });
+
+    // Clear text input
+    this.refs.input.clear();
+  };
+
+  createMessage = (text, position = "right") => ({
+    position,
+    type: "text",
+    text,
+    date: new Date()
+  });
+
   render() {
     return (
       <div>
-        <MessageList
-          lockable={true}
-          toBottomHeight={"100%"}
-          dataSource={[
-            {
-              position: "right",
-              type: "text",
-              text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
-              date: new Date()
-            },
-            {
-              position: "right",
-              type: "text",
-              text: "Blablabla",
-              date: new Date()
-            }
-          ]}
-        />
+        <MessageList dataSource={this.state.messages} />
         <Input
+          ref="input"
           multiline
-          placeholder="Type here..."
+          placeholder="Type a message..."
+          onChange={this.handleInputChange}
           rightButtons={
-            <Button color="white" backgroundColor="black" text="Send" />
+            <Button
+              color="white"
+              backgroundColor="black"
+              text="Send"
+              onClick={this.handleSendClick}
+            />
           }
         />
       </div>

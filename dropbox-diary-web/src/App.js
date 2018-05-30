4fc93057036a7dac7c1e81b5f4a39170078df823
Alex Soroka <lehaSVV2009@gmail.com>
2018-05-30 13:17:32 +0300

    Scroll messages when created

--- dropbox-diary-web/src/App.js
+++ dropbox-diary-web/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Button, Input, MessageList } from "react-chat-elements";
 
-import Layout from "./Layout";
-
 export default class App extends Component {
   constructor() {
     super();
@@ -14,30 +12,34 @@ export default class App extends Component {
 
   handleInputChange = ({ target: { value } }) => this.setState({ text: value });
 
-  handleInputKeyDown = event => {
-    // on Shift+Enter
-    if (event.keyCode === 13 && event.shiftKey) {
+  handleInputKeyUp = event => {
+    // Send message on Enter. Shift+Enter will not send message but add next line
+    if (event.keyCode === 13 && !event.shiftKey) {
       this.handleSendClick();
     }
   };
 
   handleSendClick = () => {
     const { text, messages } = this.state;
-    if (!text) {
+    if (!text || !text.trim()) {
       return;
     }
 
     // Add new message to UI
     messages.push(this.createMessage(text));
-    this.setState({
-      text: "",
-      messages
-    });
+    this.setState(
+      {
+        text: "",
+        messages
+      },
+      () => {
+        // Clear text input
+        this.refs.input.clear();
 
-    // Clear text input
-    this.refs.input.clear();
-
-    // TODO scroll to last element
+        // Scroll to last message
+        this.refs.center.scrollTop = this.refs.center.scrollHeight;
+      }
+    );
   };
 
   createMessage = (text, position = "right") => ({
@@ -49,16 +51,18 @@ export default class App extends Component {
 
   render() {
     return (
-      <Layout
-        center={<MessageList dataSource={this.state.messages} />}
-        bottom={
+      <div className="viewport">
+        <div className="center" ref="center">
+          <MessageList dataSource={this.state.messages} />
+        </div>
+        <div className="bottom">
           <Input
             className="input"
             ref="input"
             multiline
             placeholder="Type a message..."
             onChange={this.handleInputChange}
-            onKeyDown={this.handleInputKeyDown}
+            onKeyUp={this.handleInputKeyUp}
             rightButtons={
               <Button
                 color="white"
@@ -68,8 +72,8 @@ export default class App extends Component {
               />
             }
           />
-        }
-      />
+        </div>
+      </div>
     );
   }
 }

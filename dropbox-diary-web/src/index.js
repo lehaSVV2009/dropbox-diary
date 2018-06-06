a3a2b2b913c9722746405594f4c2d527c736343f
Alex Soroka <lehaSVV2009@gmail.com>
2018-06-06 07:25:16 +0300

    Add default service worker for offline access

--- dropbox-diary-web/src/index.js
+++ dropbox-diary-web/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom";
 import "react-chat-elements/dist/main.css";
 import "./index.css";
 import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
